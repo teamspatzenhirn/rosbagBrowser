a4ea3954068dabd4abb554e9452dc42cbb5df0f1
Jonas Otto <jonas@jonasotto.com>
2023-01-13 18:18:32 +0100

    deployment ansible bs

--- rosbagBrowser/settings_production.py
+++ rosbagBrowser/settings_production.py
@@ -4,14 +4,14 @@ from .settings import *
 Settings overrides for staging environment
 """
 
-ALLOWED_HOSTS = ["rosbag-browser-staging.jonasotto.com"]
+ALLOWED_HOSTS = ["rosbag.spatz.wtf"]
 DEBUG = False
-STATIC_ROOT = "/var/www/rosbagBrowser/django_static"
+STATIC_ROOT = "/django_static/static"
 ROSBAG_STORAGE_PATH = "/home/ubuntu/rosbagBrowser/rosbagsApp/testdata"
 ROSBAG_MOUNT_PATH = "/opt/aufnahmen"
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SILENCED_SYSTEM_CHECKS = [
-    "security.W004"  # We don't want to use HSTS for staging server
+    "security.W004"  # We don't want to use HSTS for any server because it literally only ever causes problems
 ]
