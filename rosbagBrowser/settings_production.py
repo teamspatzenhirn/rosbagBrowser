d277efa2daeeb9b572f9e4df37907c08c5282333
Jonas Otto <edwin.otto@uni-ulm.de>
2023-01-24 13:33:43 +0100

    fix production storage paths

--- rosbagBrowser/settings_production.py
+++ rosbagBrowser/settings_production.py
@@ -7,8 +7,8 @@ Settings overrides for staging environment
 ALLOWED_HOSTS = ["rosbag.spatz.wtf"]
 DEBUG = False
 STATIC_ROOT = "/django_static/static"
-ROSBAG_STORAGE_PATH = "/home/ubuntu/rosbagBrowser/rosbagsApp/testdata"
-ROSBAG_MOUNT_PATH = "/opt/aufnahmen"
+ROSBAG_STORAGE_PATH = "/opt/aufnahmen/2023/rosbags"
+ROSBAG_MOUNT_PATH = "/opt/aufnahmen/2023/rosbags"
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
